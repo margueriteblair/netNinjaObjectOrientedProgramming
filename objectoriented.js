956b2987b7862437fa427b92db697fbb369f27f0
Marguerite Blair <marguerite.blair@gmail.com>
2020-05-25 21:45:25 -0400

    notes on the introduction to the playlist

---
+++ objectoriented.js
@@ -0,0 +1,8 @@
+//everything in JS is an object
+//objects in JS are like objects in real life, it has things it can do, and it has properties
+//objects have properties and things they can do (methods)
+//you can find methods in the DOM, a whole list of them!!
+//arrays are an object that have properties and methods
+//window is a global object that shows us allllll the methods an object has
+//individual variables are not objects and don't have all the same properties
+//individual variables can still sometimes behave like objects
